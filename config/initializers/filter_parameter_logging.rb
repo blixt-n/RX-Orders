bc9fbd4a914dbde67a97d74179f39fd7d144f61d
Nick Blixt <206944748+blixt-n@users.noreply.github.com>
2026-07-07 14:13:10 -0500

    Filter identifying information for users and prescriptions.
    Add filters as new array to prevent accidental loss during future Rails upgrades that try to overwrite this file.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -6,3 +6,7 @@
 Rails.application.config.filter_parameters += [
   :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
 ]
+
+Rails.application.config.filter_parameters += [
+  :email, :phone_number, :medication_name
+]
